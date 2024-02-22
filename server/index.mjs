9ad254b757fee8f5c147135e2a8dc57168d9c375
Razdor13 <oneamaday@gmail.com>
2024-02-22 13:27:23 +0200

    added webSocket logic and input for chat

--- server/index.mjs
+++ server/index.mjs
@@ -2,6 +2,10 @@ import fastify from "fastify";
 import fastifyStatic from "@fastify/static";
 import path from "path";
 import { fileURLToPath } from "url";
+import { WebSocketServer } from "ws";
+
+//fastify
+
 
 const server = fastify()
 const __filename = fileURLToPath(import.meta.url)
@@ -11,9 +15,24 @@ server.register(fastifyStatic,{
     root: path.join(__dirname, '../dist')
 })
 
-
-
 server.listen({port : 5500},)
 .then(() => {
     console.log(`http://localhost:5500/`)
 })
+
+//websocket
+
+const wss = new WebSocketServer({port : 5555})
+
+wss.on('connection',client => { // event if client connection to server
+    client.on('message', (data)=> {
+        console.log('i got something', data.toString())
+        wss.clients.forEach(c => {
+            if (c !== client) {
+                c.send('other client sent ' + data)
+            }
+        })
+    })
+})
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,11 @@ import * as bootstrap from 'bootstrap'
 
 const input = document.getElementById('input-name')
 const btnSetName = document.getElementById('setname')
-const myModalforBT = new bootstrap.Modal(document.getElementById('exampleModal'));
+const myModalforBT = new bootstrap.Modal(document.getElementById('staticBackdrop'));
 const modal = document.querySelector('.modal-content')
+const anon = document.getElementById('anon')
+const btnForSend = document.getElementById('btnSend')
+const inputMsg = document.getElementById('inputMsg')
 let user 
 openModal();
 
@@ -30,6 +33,32 @@ function doShake () {
 btnSetName.addEventListener('click', () => {
   user = input.value.trim()
   user? closeModal() : doShake()
-  
-  
 })
+
+anon.addEventListener('click', () => {
+  user = 'anonimus'
+  closeModal()
+  console.log(user)
+})
+
+
+//webSocket 
+const ws = new WebSocket('ws://localhost:5555')
+
+ws.onmessage = (event) => {
+  console.log(event.data , 'got msg')
+}
+
+ws.onopen = () => {
+  btnForSend.addEventListener('click', () => {
+    const msg = inputMsg.value.trim()
+  if (msg) {
+    ws.send(msg)
+  }
+  })
+  console.log('coonect - client')
+  
+}
+ws.onclose = () => {
+  console.log('disconnected')
+}
